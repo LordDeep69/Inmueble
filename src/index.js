dfc8f68a6d74b0ec3cc0d0c56f47d29b461636e3
Anyerson Ayola <anyersonespania3@gmail.com>
2023-06-02 08:54:34 -0500

    Ceación de Repositorio

---
+++ src/index.js
@@ -0,0 +1,23 @@
+//npm init -y 
+//npm i json-server
+
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(`${__dirname}/data/db.json`);
+const middlewares = jsonServer.defaults();
+
+const port = process.env.PORT || 5000;
+
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*')
+    next()
+})
+
+server.use(router);
+server.use(middlewares)
+
+server.listen(port, () => {
+    console.log('JSON Server is running')
+})
